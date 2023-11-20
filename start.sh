a2b12eeb3e166e397ece5b6faf8bb55bd216715b
Seggellion <dustin@altama.ca>
2023-11-19 23:15:56 -0800

    debugging

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@
 # start.sh4
 
 echo "Debug: Content of GOOGLE_CREDENTIALS:"
-echo "$GOOGLE_CREDENTIALS"
+
 
 # Create the Google credentials file
-echo "$GOOGLE_CREDENTIALS" > google-credentials.json
+echo "$GOOGLE_CREDENTIALS" > /app/google-credentials.json
+export GOOGLE_APPLICATION_CREDENTIALS=/app/google-credentials.json
 
 # Set GOOGLE_APPLICATION_CREDENTIALS environment variable
 # export GOOGLE_APPLICATION_CREDENTIALS=/app/google-credentials.json
-
-# List files in /app directory for debugging
-ls -l /app
-
 rm -f tmp/pids/server.pid
 
 
 # Start Rails Server
 bundle exec rails s -p 3000 -b '0.0.0.0'
+
+exec "$@"
+
