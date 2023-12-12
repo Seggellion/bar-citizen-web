30f2b327e974759a4a19b3efbd69c085598ef31d
Seggellion <dustin@altama.ca>
2023-12-12 15:53:34 -0800

    hotfix

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -2,30 +2,45 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["carouselSlide"]
-
   currentIndex = 0
+  interval = null
 
   connect() {
-    if (this.carouselSlideTargets.length > 0) {
-      this.showSlide(this.currentIndex)
-    }
+    this.showSlide(this.currentIndex)
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.interval = setInterval(() => {
+      this.next()
+    }, 5000); // Change slide every 5 seconds
+  }
+
+  stopAutoSlide() {
+    clearInterval(this.interval);
   }
 
   next() {
-    console.log('next');
-
+    this.stopAutoSlide();
     this.showSlide((this.currentIndex + 1) % this.carouselSlideTargets.length)
+    this.startAutoSlide();
   }
 
   previous() {
-    console.log('previous');
+    this.stopAutoSlide();
     this.showSlide((this.currentIndex - 1 + this.carouselSlideTargets.length) % this.carouselSlideTargets.length)
+    this.startAutoSlide();
   }
 
   showSlide(index) {
     this.currentIndex = index
     this.carouselSlideTargets.forEach((el, i) => {
-      el.classList.toggle("hidden", i !== index)
+      el.classList.toggle("hidden", i !== this.currentIndex)
+      el.classList.toggle("slide-in", i === this.currentIndex)
     })
+  }
+
+  disconnect() {
+    this.stopAutoSlide();
   }
 }
