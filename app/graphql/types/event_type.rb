cca135680368e4254f9f56aa5e6f2e857e0e98ad
Seggellion <dustin@altama.ca>
2024-01-23 12:58:09 -0800

    hotfix removed null from eventtype

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -2,8 +2,8 @@ module Types
     class EventType < Types::BaseObject
       field :title, String, null: false
       field :id, Integer, null: false
-      field :latitude, Float, null: false
-      field :longitude, Float, null: false
+      field :latitude, Float
+      field :longitude, Float
       field :description, String, null: true
       field :start_datetime, GraphQL::Types::ISO8601DateTime, null: true
       field :end_datetime, GraphQL::Types::ISO8601DateTime, null: true
