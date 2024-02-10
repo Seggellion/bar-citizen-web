d3bd4d5ecb70bd6244dac8b35bbea0c98070a5fd
Seggellion <dustin@altama.ca>
2024-02-10 07:51:01 -0800

    forgot to add email usertype field

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,6 +3,7 @@ module Types
       field :id, ID, null: false # Make sure this line exists
       field :username, String, null: true
       field :bio, String, null: true
+      field :email, String, null: true
       field :location, String, null: true
       field :title, String, null: true
       field :profile_image, String, null: true
